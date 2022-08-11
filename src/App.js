f22fdd20179881dda73c0084501f47c188b471ba
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-11 17:55:59 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [number,setNumber]=useState(0);
+  console.log("Compoment App is rendering");
+  // console.count("Times rendered");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>You Clicked{number}</span>
+      <button onClick={()=>setNumber(number+1)}>
+        Click and Increment
+        </button>
     </div>
   );
 }
